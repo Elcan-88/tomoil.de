fc0db44bc8551ee05c5eb457c827bad2b3b5fad9
Elcan-88 <eljan.hamidov@gmail.com>
2022-08-28 05:08:36 +0400

    Latest version

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,14 @@
 $(document).ready(function(){
     $('.question > .question-item > .question-item__body').hide();
       
-    $('.question > .question-item').click(function() {
-      if ($(this).hasClass("active")) {
-        $(this).removeClass("active").find(".question-item__body").slideUp();
+    $('.question > .question-item .question-item__header').click(function() {
+      const item = $(this).parent();
+      if (item.hasClass("active")) {
+        item.removeClass("active").find(".question-item__body").slideUp();
       } else {
         $(".question > .question-item.active .question-item__body").slideUp();
         $(".question > .question-item.active").removeClass("active");
-        $(this).addClass("active").find(".question-item__body").slideDown();
+        item.addClass("active").find(".question-item__body").slideDown();
       }
       return false;
     });
